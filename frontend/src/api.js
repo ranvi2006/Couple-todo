ee3611289d569bad3b987369e34235a0a6712c31
ranvi2006 <rajurk2002@gmail.com>
2025-08-19 16:20:09 +0530

    done

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3000/api", // backend URL
+  baseURL: "https://couple-todo-psa2.onrender.com", // backend URL
 });
 
 export default API;
